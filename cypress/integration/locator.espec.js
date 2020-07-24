ecf7fa39f08f9c5a32210bddfa1439a0a1af954e
Anderson Leite <andersondias@cc.ci.ufpb.br>
2020-07-24 02:26:30 -0300

    Add Xpath_new

--- cypress/integration/locator.espec.js
+++ cypress/integration/locator.espec.js
@@ -10,10 +10,10 @@ describe('Locator', ()=>{
     })
 
     it('JQuery selector', ()=>{
-        cy.get('table#tabelaUsuarios tbody tr td:eq(6) input').type('Anderson')
+        cy.get('table#tabelaUsuarios tbody > tr > td:eq(0)').should('contain', 'Francisco')
     })
 
-    it ('Using xpath', ()=>{
+    it('Using xpath',()=>{
         cy.xpath('//input')
     })
 
